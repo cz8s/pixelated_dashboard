8a3f4fe3247022a354bc1d6d3e79dd40cedfd3a9
Christoph <chris@inferno.nadir.org>
2015-07-05 23:55:32 +0200

    send activity and overall status to widget

--- jobs/user-agent-snap.rb
+++ jobs/user-agent-snap.rb
@@ -5,14 +5,16 @@ steps = Hash.new({ value: 0 })
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
   begin
+    overallstatus = 'ok'
     snap_xml = Nokogiri::XML(open("https://snap-ci.com/pixelated-project/pixelated-user-agent/branch/master/cctray.xml"))
     snap_xml.xpath('//Project').each do |p|
       name = p.attribute('name').value.split(' ').last
       status = p.attribute('lastBuildStatus')
+      overallstatus='warning' if status.to_s != 'Success'
       activity = p.attribute('activity')
-      steps[name] =  { label: name, status: status}
+      steps[name] =  { label: name, status: status, activity: activity}
     end
-    send_event('user-agent', { items: steps.values })
+    send_event('user-agent', { items: steps.values , status: overallstatus })
   end
 end
 
