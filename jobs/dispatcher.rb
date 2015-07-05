0f841bc55e08afd500fb708c24f33ca23a7f8c52
Christoph <chris@inferno.nadir.org>
2015-07-05 22:59:00 +0200

    jobs for ccmenu

---
+++ jobs/dispatcher.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+
+steps = Hash.new({ value: 0 })
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  begin
+    snap_xml = Nokogiri::XML(open("https://snap-ci.com/pixelated-project/pixelated-dispatcher/branch/master/cctray.xml"))
+    snap_xml.xpath('//Project').each do |p|
+      name = p.attribute('name').value.split(' ').last
+      status = p.attribute('lastBuildStatus')
+      activity = p.attribute('activity')
+      steps[name] =  { label: name, value: status}
+    end
+    send_event('dispatcher', { items: steps.values })
+  end
+end
+
