fa007e303fc1a5a047a18cfc7259778e02120015
Christoph <chris@inferno.nadir.org>
2015-09-05 19:11:59 -0300

    add second ccmenu from snap

--- jobs/ccmenu.rb
+++ jobs/ccmenu.rb
@@ -1,9 +1,16 @@
 require './lib/ccmenu'
 
 SCHEDULER.every '3s', :first_in => 0 do |job|
-    a = CCMenu.new '/home/chris/cctray.xml' 
-    send_event('count', {current:a.count})
-    send_event('failed', {text:a.failed})
-    send_event('numfailed', {current:a.failed.length})
+    go = CCMenu.new '/home/chris/cctray.xml' 
+    send_event('go_count', {current:go.count})
+    send_event('go_failed', {text:go.failed})
+    send_event('go_numfailed', {current:go.failed.length})
+end
+
+SCHEDULER.every '3s', :first_in => 0 do |job|
+    snap = CCMenu.new '/home/chris/work/cctray.xml' 
+    send_event('snap_count', {current:snap.count})
+    send_event('snap_failed', {text:snap.failed})
+    send_event('snap_numfailed', {current:snap.failed.length})
 end
 
