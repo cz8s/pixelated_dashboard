778c0547121dcba6f37a82f241c4ab8a63f50c17
Christoph <chris@inferno.nadir.org>
2015-09-05 18:38:05 -0300

    show less info. for smaller screens

--- jobs/ccmenu.rb
+++ jobs/ccmenu.rb
@@ -5,6 +5,5 @@ SCHEDULER.every '3s', :first_in => 0 do |job|
     send_event('count', {current:a.count})
     send_event('failed', {text:a.failed})
     send_event('numfailed', {current:a.failed.length})
-    send_event('json', {items:a.values,color:a.color})
 end
 
